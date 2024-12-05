ade7fa54c289a14c52963ec5b063914fc65894c3
mutaz-tenbekji <tenbakji.realestate@gmail.com>
2024-12-05 13:38:13 +0300

    edit on contact form

--- resources/views/components/contact-form.blade.php
+++ resources/views/components/contact-form.blade.php
@@ -48,7 +48,7 @@ document.addEventListener('DOMContentLoaded', function() {
             @enderror
         </div>
         <div class="mb-4">
-            <input type="text" name="phone_number" id="phone_number" placeholder="رقم اهاتف"
+            <input type="text" name="phone_number" id="phone_number" placeholder="رقم الهاتف"
                 class="mt-1 block w-1/3 bg-gray-100 placeholder:text-black placeholder:text-sm mx-auto border border-gray-300 rounded-md p-2 focus:outline-none focus:ring focus:ring-orange-300"
                 required>
             @error('phone_number')
